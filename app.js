54306cd0fcdc355a2fd3e2f00bab3bfa85b81897
Sohil Salim <sohiltuttu@gmail.com>
2026-09-22 02:23:27 +0530

    Set maxHttpBufferSize for socket.io
    Increased max HTTP buffer size for socket.io to 200 MB.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    maxHttpBufferSize: 200 * 1024 * 1024 // 200 MB
+});
 
 app.use(express.static('public'));
 
